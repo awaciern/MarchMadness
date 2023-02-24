bf3f6ea903ae288b332c2839b3d71ecc627a81cf
awaciern <awaciern@ncsu.edu>
2023-02-24 01:05:10 -0500

    Starting to reconcile names across data

--- Python/compile_games.py
+++ Python/compile_games.py
@@ -3,6 +3,7 @@ import pandas as pd
 # j = 0
 df_all_games = pd.DataFrame()
 for year in range(2012, 2023):
+    df_year_games = pd.DataFrame()
     if year == 2020:
         continue
     for round in range(1, 7):
@@ -14,7 +15,9 @@ for year in range(2012, 2023):
         # print(df)
         # print('YEAR: ' + str(year) + ', ROUND: ' + str(round) + ', i = ' + str(i))
         df_all_games = pd.concat([df_all_games, df])
+        df_year_games = pd.concat([df_year_games, df])
+    df_year_games.to_csv(path_or_buf='..\\Data\\GameData\\' + str(year) + '.csv', index=False)
 
 print(df_all_games)
-df_all_games.to_csv(path_or_buf='..\\Data\\GameData\\AllGames.csv', index=False)
+df_all_games.to_csv(path_or_buf='..\\Data\\GameData\\All.csv', index=False)
 # print(j)
---
+++ Python/find_name_mismatch.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+games_mismatch_names = []
+kp_mismatch_names = []
+for year in range(2012, 2022):
+    if year == 2020:
+        continue
+    df_games = pd.read_csv('..\\Data\\GameData\\' + str(year) + '.csv')
+    df_kp = pd.read_csv('..\\Data\\KenPomData\\' + str(year) + '.csv')
+    # print(df_bracket)
+    # print(df_kp['Team'].values)
+    for index, row in df_games.iterrows():
+        team1 = row['Team1']
+        # print(team1)
+        # print(df_kp.loc[df_kp['Team'] == team1])
+        if team1 not in df_kp['Team'].values and \
+           team1 not in games_mismatch_names:
+            games_mismatch_names.append(team1)
+
+        team2 = row['Team2']
+        # print(team2)
+        # print(df_kp.loc[df_kp['Team'] == team2])
+        # print()
+        if team2 not in df_kp['Team'].values and \
+           team2 not in games_mismatch_names:
+            games_mismatch_names.append(team2)
+
+    for index, row in df_kp.iterrows():
+        team = row['Team']
+        if team not in df_games['Team1'].values and \
+           team not in df_games['Team2'].values and \
+           team not in kp_mismatch_names:
+            kp_mismatch_names.append(team)
+
+print('TEAM NAMES IN GAMES BUT NOT IN KENPOM (length=' + str(len(games_mismatch_names)) + '):')
+print(pd.Series(games_mismatch_names).sort_values())
+print()
+print('TEAM NAMES IN KENPOM BUT NOT IN GAMES (length=' + str(len(kp_mismatch_names)) + '):')
+print(pd.Series(kp_mismatch_names).sort_values())
